926de8a7d11b7756afebc3b583f8ebcebebce605
Ruben Heymans <ruben.heymans@gmail.com>
2014-10-08 12:06:46 +0200

    Update analytics

--- models/Connect.php
+++ models/Connect.php
@@ -18,12 +18,12 @@ class Connect extends \yii\base\Action
 
         $client = new \Google_Client();
         $client->setApplicationName("API Project");
-        $client->setDeveloperKey('AIzaSyDitQlwUtlc-ar17NXRutYoYfmskHiCuS4');
+        $client->setDeveloperKey('');
 
         // Add this email address as a new user to your Google analytics property
-        $serviceAccountName = '254715720101-b7kcs7a23oveeuhobb33o6clmrles2rt@developer.gserviceaccount.com';
+        $serviceAccountName = '';
         $scopes = ['https://www.googleapis.com/auth/analytics.readonly'];
-        $key = file_get_contents(Yii::getAlias('@webroot') . '/certificate/API Project-67815eac03a5.p12');
+        $key = file_get_contents(Yii::getAlias('@webroot') . '/certificate/');
 
         $cred = new \Google_Auth_AssertionCredentials(
             $serviceAccountName,
@@ -33,7 +33,7 @@ class Connect extends \yii\base\Action
 
         $client->setAssertionCredentials($cred);
 
-        $clientId = '254715720101-b7kcs7a23oveeuhobb33o6clmrles2rt.apps.googleusercontent.com';
+        $clientId = '';
         $client->setClientId($clientId);
         $client->setAccessType('offline_access');
 
