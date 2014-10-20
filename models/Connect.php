e78d9feb3f0bc02735993f105b93715aba1817d2
Fabio Maggio <fabio@infoweb.be>
2014-10-20 16:08:43 +0200

    Implement caching to GA connection class
    Fix #6

--- models/Connect.php
+++ models/Connect.php
@@ -17,15 +17,22 @@ class Connect extends \yii\base\Action
     public $endDate;
     public $connection = null;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::init();
         
-        // Create the connection
-        $this->connectToAnalytics();
+        // Read the connection from cache
+        $connection = $this->readFromCache('infoweb/analytics/connection');
+        
+        if (!$connection) {
+            $this->connectToAnalytics(); 
+        } else {
+            $this->connection = $connection;       
+        }
     }
 
-    public function connectToAnalytics() {
-
+    public function connectToAnalytics()
+    {       
         if ($this->connection == null) {
             require_once Yii::getAlias('@google/api') . '/Google/Client.php';
             require_once Yii::getAlias('@google/api') . '/Google/Service/Analytics.php';
@@ -50,6 +57,9 @@ class Connect extends \yii\base\Action
             $analytics = new \Google_Service_Analytics($client);
     
             $this->connection = $analytics;
+            
+            // Cache the connection
+            $this->cacheData('infoweb/analytics/connection', $this->connection);
         }
 
         return $this->connection;
@@ -60,21 +70,28 @@ class Connect extends \yii\base\Action
      *
      * @return string
      */
-    public function getSessions() {
-
-        try {
-            // You can find your analytics profile id here: Admin -> Profile Settings -> Profile ID
-            $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['dimensions' => 'ga:date']);
-
-            $data[] = [Yii::t('infoweb/analytics', 'Day'), Yii::t('infoweb/analytics', 'Sessions')];
-
-            foreach ($results['rows'] as $result)
-            {
-                $data[] = [date('d-m-Y', strtotime($result[0])), (int)$result[1]];
+    public function getSessions()
+    {
+        // Read the data from cache
+        $data = $this->readFromCache('infoweb/analytics/data/sessions');
+        
+        if (!$data) {               
+            try {
+                // You can find your analytics profile id here: Admin -> Profile Settings -> Profile ID
+                $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['dimensions' => 'ga:date']);
+    
+                $data[] = [Yii::t('infoweb/analytics', 'Day'), Yii::t('infoweb/analytics', 'Sessions')];
+    
+                foreach ($results['rows'] as $result)
+                {
+                    $data[] = [date('d-m-Y', strtotime($result[0])), (int)$result[1]];
+                }
+                
+                $this->cacheData('infoweb/analytics/data/sessions', $data);
+    
+            } catch(Exception $e) {
+                throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
             }
-
-        } catch(Exception $e) {
-            throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
         }
 
         return json_encode($data);
@@ -85,20 +102,26 @@ class Connect extends \yii\base\Action
      *
      * @return string
      */
-    public function getVisitors() {
-
-        try {
-
-            $results['returningVisitors'] = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['segment' => 'gaid::-3'])->getTotalsForAllResults();
-            $results['newVisitors'] = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['segment' => 'gaid::-2'])->getTotalsForAllResults();
-
-            $data[] = [Yii::t('infoweb/analytics', 'Title'), Yii::t('infoweb/analytics', 'Total')];
-
-            $data[] = [Yii::t('infoweb/analytics', 'Returning visitor'), (int)$results['returningVisitors']['ga:sessions']];
-            $data[] = [Yii::t('infoweb/analytics', 'New visitor'), (int)$results['newVisitors']['ga:sessions']];
-
-        } catch(Exception $e) {
-            throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+    public function getVisitors()
+    {
+        // Read the data from cache
+        $data = $this->readFromCache('infoweb/analytics/data/visitors');
+        
+        if (!$data) { 
+            try {
+    
+                $results['returningVisitors'] = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['segment' => 'gaid::-3'])->getTotalsForAllResults();
+                $results['newVisitors'] = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['segment' => 'gaid::-2'])->getTotalsForAllResults();
+    
+                $data[] = [Yii::t('infoweb/analytics', 'Title'), Yii::t('infoweb/analytics', 'Total')];
+    
+                $data[] = [Yii::t('infoweb/analytics', 'Returning visitor'), (int)$results['returningVisitors']['ga:sessions']];
+                $data[] = [Yii::t('infoweb/analytics', 'New visitor'), (int)$results['newVisitors']['ga:sessions']];
+                
+                $this->cacheData('infoweb/analytics/data/visitors', $data);
+            } catch(Exception $e) {
+                throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+            }
         }
 
         return json_encode($data);
@@ -109,28 +132,35 @@ class Connect extends \yii\base\Action
      *
      * @return string
      */
-    public function getCountries() {
-
-        try {
-
-            //$results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['sort' => '-ga:sessions', 'max-results' => 10]);
-            $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['dimensions' => 'ga:country', 'sort' => '-ga:sessions', 'max-results' => 10]);
-
-            $data['cols'] = [
-                ['id' => 'countries', 'label' => Yii::t('infoweb/analytics', 'Countries'), 'type' => 'string'],
-                ['id' => 'sessions', 'label' => Yii::t('infoweb/analytics', 'Sessions'), 'type' => 'number'],
-            ];
-
-            foreach ($results['rows'] as $result)
-            {
-                $data['rows'][] = ['c' => [
-                    ['v' => $result[0]],
-                    ['v' => (int)$result[1]]],
+    public function getCountries()
+    {
+        // Read the data from cache
+        $data = $this->readFromCache('infoweb/analytics/data/countries');
+        
+        if (!$data) { 
+            try {
+    
+                //$results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['sort' => '-ga:sessions', 'max-results' => 10]);
+                $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions', ['dimensions' => 'ga:country', 'sort' => '-ga:sessions', 'max-results' => 10]);
+    
+                $data['cols'] = [
+                    ['id' => 'countries', 'label' => Yii::t('infoweb/analytics', 'Countries'), 'type' => 'string'],
+                    ['id' => 'sessions', 'label' => Yii::t('infoweb/analytics', 'Sessions'), 'type' => 'number'],
                 ];
+    
+                foreach ($results['rows'] as $result)
+                {
+                    $data['rows'][] = ['c' => [
+                        ['v' => $result[0]],
+                        ['v' => (int)$result[1]]],
+                    ];
+                }
+                
+                $this->cacheData('infoweb/analytics/data/countries', $data);
+    
+            } catch(Exception $e) {
+                throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
             }
-
-        } catch(Exception $e) {
-            throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
         }
 
         return json_encode($data);
@@ -141,15 +171,21 @@ class Connect extends \yii\base\Action
      *
      * @return string
      */
-    public function getTotalSessions() {
-
-        try {
-            $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions')->getTotalsForAllResults();
-
-            $data = number_format($results['ga:sessions'], 0, ',', '.');
-
-        } catch(Exception $e) {
-            throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+    public function getTotalSessions()
+    {
+        // Read the data from cache
+        $data = $this->readFromCache('infoweb/analytics/data/totalSessions');
+        
+        if (!$data) { 
+            try {
+                $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:sessions')->getTotalsForAllResults();
+    
+                $data = number_format($results['ga:sessions'], 0, ',', '.');
+                
+                $this->cacheData('infoweb/analytics/data/totalSessions', $data);
+            } catch(Exception $e) {
+                throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+            }
         }
 
         return json_encode($data);
@@ -160,15 +196,21 @@ class Connect extends \yii\base\Action
      *
      * @return string
      */
-    public function getTotalUsers() {
-
-        try {
-            $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:users')->getTotalsForAllResults();
-
-            $data = number_format($results['ga:users'], 0, ',', '.');
-
-        } catch(Exception $e) {
-            throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+    public function getTotalUsers()
+    {
+        // Read the data from cache
+        $data = $this->readFromCache('infoweb/analytics/data/totalUsers');
+        
+        if (!$data) { 
+            try {
+                $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:users')->getTotalsForAllResults();
+    
+                $data = number_format($results['ga:users'], 0, ',', '.');
+                
+                $this->cacheData('infoweb/analytics/data/totalUsers', $data);
+            } catch(Exception $e) {
+                throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+            }
         }
 
         return json_encode($data);
@@ -179,15 +221,21 @@ class Connect extends \yii\base\Action
      *
      * @return string
      */
-    public function getTotalPageViews() {
-
-        try {
-            $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:pageviews')->getTotalsForAllResults();
-
-            $data = number_format($results['ga:pageviews'], 0, ',', '.');
-
-        } catch(Exception $e) {
-            throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+    public function getTotalPageViews()
+    {
+        // Read the data from cache
+        $data = $this->readFromCache('infoweb/analytics/data/totalPageViews');
+        
+        if (!$data) { 
+            try {
+                $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:pageviews')->getTotalsForAllResults();
+    
+                $data = number_format($results['ga:pageviews'], 0, ',', '.');
+                
+                $this->cacheData('infoweb/analytics/data/totalPageViews', $data);
+            } catch(Exception $e) {
+                throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+            }
         }
 
         return json_encode($data);
@@ -198,20 +246,52 @@ class Connect extends \yii\base\Action
      *
      * @return string
      */
-    public function getAverageSessionLength() {
-
-        try {
-            $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:avgSessionDuration')->getTotalsForAllResults();
-
-            $data = gmdate('H:i:m', $results['ga:avgSessionDuration']);
-
-
-        } catch(Exception $e) {
-            throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+    public function getAverageSessionLength()
+    {
+        // Read the data from cache
+        $data = $this->readFromCache('infoweb/analytics/data/averageSessionLength');
+        
+        if (!$data) { 
+            try {
+                $results = $this->connection->data_ga->get(Yii::$app->params['analytics']['analyticsId'], $this->startDate, $this->endDate, 'ga:avgSessionDuration')->getTotalsForAllResults();
+    
+                $data = gmdate('H:i:m', $results['ga:avgSessionDuration']);
+    
+                $this->cacheData('infoweb/analytics/data/averageSessionLength', $data);
+            } catch(Exception $e) {
+                throw new BaseException(Yii::t('infoweb/analytics', 'Error while fetching data from Google Analytics'));
+            }
         }
 
         return json_encode($data);
     }
 
-
+    /**
+     * Caches data in a session variable
+     * 
+     * @param   string  $key        The key that has to be used in the session var
+     * @param   mixed   $data
+     * @return  boolean
+     */
+    public function cacheData($key = '', $data = [])
+    {
+        $session = Yii::$app->session;
+        
+        $session[$key] = serialize($data);
+        
+        return true;    
+    }
+    
+    /**
+     * Reads the data from cache
+     * 
+     * @param   string  $key
+     * @return  mixed
+     */
+    public function readFromCache($key)
+    {
+        $session = Yii::$app->session;
+        
+        return ($session->has($key)) ? unserialize($session[$key]) : [];     
+    }
 }
